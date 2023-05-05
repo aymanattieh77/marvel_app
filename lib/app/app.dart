b2dc502649538ef1442147708939717002dbc282
ayman attieh <aymanattieh77@gmail.com>
2023-05-05 20:48:09 +0300

    update folders

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MarvelApp extends StatelessWidget {
   const MarvelApp({super.key});
-
+// the root of Application
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
