0e3182a26eda82e4b6368b58ec712df1e381baf1
ayman attieh <aymanattieh77@gmail.com>
2023-05-05 22:39:40 +0300

    update app strings

---
+++ lib/app/res/strings.dart
@@ -0,0 +1,124 @@
+abstract class AppStrings {
+  // oboarding, signUp, and login strings
+  static const String onBoardingTitle1 =
+      'All your favourite MARVEL Movies & Series at one place';
+  static const String onBoardingTitle2 = 'Watch Online\nor\nDownload Offline';
+  static const String onBoardingTitle3 =
+      'Create profiles for diffrent members & get personalised recommendations';
+
+  static const String onBoardingTitle4 =
+      'Plans according to your needs at affordable prices';
+  static const String onBoardingTitle5 = 'Let’s Get Started !!!';
+  static const String continue_ = 'Continue';
+  static const String signup = 'Signup';
+  static const String login = 'Login';
+  static const String forgotPassword = 'Forgot Password?';
+  static const String or = 'or';
+  static const String continueWith = 'Continue With';
+  static const String alreadyHaveAccount = 'Already have an account';
+  static const String dontHaveAccount = 'Don’t have an account';
+  static const String show = 'Show';
+  static const String password = 'Password';
+  static const String emailID = 'Enter your Email ID';
+  static const String facebook = 'Facebook';
+  static const String google = 'Google';
+
+  // payment strings
+  static const String chooseYourPlan = 'Choose your Plan';
+  static const String cancelAtAnyTime = 'Cancel at any time';
+  static const String moviesAndSeries = 'Movies & Series';
+  static const String moviesAndSeriesPrice = '\$20/mth';
+  static const String movies = 'Movies';
+  static const String moviesPrice = '\$15/mth';
+  static const String series = 'Series';
+  static const String seriesPrice = '\$15/mth';
+  static const String chooseHowToPay = 'Choose how to pay';
+  static const String creditDebitCard = 'Credit / Debit Card';
+  static const String netbanking = 'Netbanking';
+  static const String change = 'Change';
+  static const String fillYourCreditCardDetails =
+      'Fill your Credit / Debit Card Details';
+  static const String firstName = 'First Name';
+  static const String lastName = 'Last Name';
+  static const String cardNumber = 'Card Number';
+  static const String expirationDate = 'Expiration Date (MM/YY)';
+  static const String securityCodeCvv = 'Security Code (CVV)';
+  static const String enterYourOTP = 'Enter your OTP';
+  static const String yourOTPNumber =
+      'Please enter the OTP that we’ve sent on your phone number 55XXXXXX99 linked with your bank account.';
+  static const String didntReceveTheOTP = 'Didn’t receve the OTP?';
+  static const String verifyingPayment = 'Verifying Payment';
+  static const String pleaseWaitVerifyYourPayment =
+      'Please wait while we verify your payment from the bank...';
+  static const String paymentVerified = 'Payment Verified';
+
+  // profile Strings
+
+  static const String chooseYourAvatar = 'Choose your Avatar';
+  static const String looksGood = 'Looks Good';
+  static const String enterYourUsername = 'Enter your Username';
+  static const String username = 'Username';
+  static const String usernameIsAvailable = 'Username is available';
+  static const String usernameCheck =
+      'Minimum 8 characters\nOnly A-Z, a-z, 0-9\n1 special charcter (. , _ - / &)';
+  static const String callMeThis = 'Call me this';
+  static const String createAPassword = 'Create a password';
+  static const String chooseAPassword = 'Choose a Password';
+  static const String looksStrong = 'Looks Strong';
+  static const String createAPin = 'Create a Pin';
+  static const String allSafeNow = 'I’m all safe now';
+  static const String yourProfileCreatedSuccessfully =
+      'Your Profile is Created Successfully!!';
+  static const String eatYourGreenVegitables = 'Eat Your Green Vegitables';
+  static const String pinUsed =
+      'This pin will be used to log-in to your profile';
+
+// home strings
+  static const String lastetMovies = 'Latest Movies';
+  static const String lastetSeries = 'Latest Series';
+  static const String trendingToday = 'Trending Today';
+  static const String home = 'Home';
+  static const String categories = 'Categories';
+  static const String downloads = 'Downloads';
+  static const String download = 'Download';
+  static const String trailer = 'Trailer';
+  static const String more = 'More';
+  static const String cast = 'Cast';
+  static const String addToWatchlist = '+Add to Watchlist';
+  static const String watchlist = 'Watchlist';
+  static const String watched = 'watchlist';
+  static const String gb = 'GB';
+  static const String deleteThisDownloadContent =
+      'Delete this Downloaded content?';
+  static const String yes = 'Yes';
+  static const String cancel = 'Cancel';
+
+// more strings
+
+  static const String account = 'Account';
+  static const String settings = 'Settings';
+  static const String legal = 'Legal';
+  static const String support = 'Support';
+  static const String privacySettings = 'Privacy Settings';
+  static const String parentalControl = 'Parental Control';
+  static const String signOut = 'Sign Out';
+  static const String generalSettings = 'General Settings';
+  static const String autoplay = 'Autoplay';
+  static const String pushNotifications = 'Push Notifications';
+  static const String downloadPreferences = 'Download Preferences';
+  static const String autodeleteUponCompletion = 'Autodelete upon completion';
+  static const String downloadWifi = 'Download only with Wi-Fi';
+  static const String deleteAllDownload = 'Delete all downloads';
+  static const String downloadVideoQuality = 'Download Video Quality';
+  static const String highDefinition = 'High Definition';
+  static const String usesMoreData = 'Uses more data';
+  static const String standardDefinition = 'Standard Definition';
+  static const String usesLessData = 'Uses less data';
+  static const String mobileStorage = 'Mobile Storage';
+  static const String used = 'Used';
+  static const String marvel = 'Marvel';
+  static const String free = 'Free';
+  static const String whoWatching = 'Who’s Watching?';
+  static const String createProfile = 'Create Profile';
+  static const String doYouReallySignOut = 'Do you really want to Sign Out?';
+}
