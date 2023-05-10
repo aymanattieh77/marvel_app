dfa93b9beda0ec2d8d7cb323b9218d43c9381a54
ayman attieh <aymanattieh77@gmail.com>
2023-05-10 19:20:29 +0300

    update download watchlist page

--- lib/presentaion/main/downloads/pages/watch_list_download_page.dart
+++ lib/presentaion/main/downloads/pages/watch_list_download_page.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:marvel_app/app/res/strings.dart';
-import 'package:marvel_app/app/res/styles.dart';
 import 'package:marvel_app/app/res/values.dart';
 import 'package:marvel_app/presentaion/common/widgets/custom_animated_toggle.dart';
+import 'package:marvel_app/presentaion/main/categories/widgets/categories_movie_series_girdview.dart';
 import 'package:marvel_app/presentaion/main/downloads/widgets/movie_series_download_listview.dart';
 import 'package:marvel_app/presentaion/main/home/widgets/home/custom_home_appbar.dart';
 
-class WatchListDownloadPage extends StatelessWidget {
+class WatchListDownloadPage extends StatefulWidget {
   const WatchListDownloadPage({super.key});
 
+  @override
+  State<WatchListDownloadPage> createState() => _WatchListDownloadPageState();
+}
+
+class _WatchListDownloadPageState extends State<WatchListDownloadPage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,11 +24,13 @@ class WatchListDownloadPage extends StatelessWidget {
         CustomAnimatedToggle(
             values: const [AppStrings.download, AppStrings.watchlist],
             onPress: (index) {
-              print(index);
+              setState(() {
+                currentIndex = index;
+              });
             }),
         const SizedBox(height: AppSizes.s16),
-        const MovieSeriesDownloadListview(),
-        //todo const CategoriesMovieSeriesGridview(),
+        if (currentIndex == 0) const MovieSeriesDownloadListview(),
+        if (currentIndex == 1) const CategoriesMovieSeriesGridview(),
       ],
     );
   }
